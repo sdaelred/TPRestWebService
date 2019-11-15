729da751a7fa951889ecdc4bc060ec0d2700c09e
Aelred <sdaelred@gmail.com>
2019-11-15 15:16:45 +0100

    Commit initial

---
+++ src/main/java/fr/efrei/TPRestWebService/TpRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.TPRestWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/efrei/TPRestWebService/TpRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.TPRestWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
